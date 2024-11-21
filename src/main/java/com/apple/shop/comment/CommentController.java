8bfbdf7d13d15275eef98c36ea46671d95f5480c
JJAJJAKIM <csm9121@gmail.com>
2024-11-21 23:53:05 +0900

    [리뷰] 상세페이지 리뷰 등록 api 완료

--- src/main/java/com/apple/shop/comment/CommentController.java
+++ src/main/java/com/apple/shop/comment/CommentController.java
@@ -1,16 +1,23 @@
 package com.apple.shop.comment;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class CommentController {
 
+    private final CommentRepository commentRepository;
+
     @PostMapping("/addComment")
-    public String addComment(Comment comment, Authentication authentication ) {
+    public String addComment(@ModelAttribute Comment comment, Authentication auth ) {
+        comment.setUsername(auth.getName());
+        commentRepository.save(comment);
         return "redirect:/detail/"+ comment.getParentId();
     }
 }
